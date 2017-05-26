9de960fd32bba76eb4554c1b2bf1ea9fea38b512
Sebastien Jourdain <sebastien.jourdain@kitware.com>
2017-05-26 16:26:45 -0600

    Start interface with ITK

--- src/dataHandler.js
+++ src/dataHandler.js
@@ -1,4 +1,8 @@
 import vtkHttpDataAccessHelper from 'vtk.js/Sources/IO/Core/DataAccessHelper/HttpDataAccessHelper';
+import vtkImageData from 'vtk.js/Sources/Common/DataModel/ImageData';
+import vtkDataArray from 'vtk.js/Sources/Common/Core/ImageData';
+
+import NrrdReader from 'itk/dist/itkImageIOs/itkNrrdImageIOJSBinding';
 
 import viewer from './viewer';
 
@@ -7,10 +11,45 @@ function fetchZip(url) {
 }
 
 function processData(container, { file, ext }) {
+  /* eslint-disable new-cap */
+  // FIXME --------------------------------------------------------------------
+  const filePath = '';
+  NrrdReader.mountContainingDirectory(filePath);
+
+  const reader = new NrrdReader.ITKImageIO();
+  reader.SetFileName(filePath);
+  // reader.SetFileContent(file); // <--- Something like that for web
+  reader.ReadImageInformation();
+
+  const array = {
+    values: reader.Read(),
+    numberOfComponents: reader.GetNumberOfComponents(),
+  };
+
+  const image = {
+    origin: [0, 0, 0],
+    spacing: [1, 1, 1],
+  };
+
+  const dimensions = [1, 1, 1];
+
+  for (let idx = 0; idx < reader.GetNumberOfDimensions(); idx++) {
+    image.origin[idx] = reader.GetOrigin(idx);
+    image.spacing[idx] = reader.GetSpacing(idx);
+    dimensions[idx] = reader.GetDimensions(idx);
+  }
+  NrrdReader.unmountContainingDirectory(filePath);
+
+  // Create VTK Image Data
+  const imageData = vtkImageData.newInstance(image);
+  const scalar = vtkDataArray.newInstance(array);
+  imageData.setDimension(...dimensions);
+  imageData.getPointData().setScalars(scalar);
+  // FIXME --------------------------------------------------------------------
+
   viewer.createViewer(container, {
-    type: 'rawFile',
-    file,
-    ext,
+    type: 'volumeRenderering',
+    image: imageData,
   });
 }
 

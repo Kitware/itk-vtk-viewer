8974f8c31dc85d7a2696569d6443b182b7a56637
Matt McCormick <matt.mccormick@kitware.com>
2017-06-30 14:39:59 -0400

    feat(itk-vtk-bridge): apply direction read into itk image to vtk image

--- src/dataHandler.js
+++ src/dataHandler.js
@@ -2,6 +2,8 @@ import vtkImageData from 'vtk.js/Sources/Common/DataModel/ImageData';
 import vtkDataArray from 'vtk.js/Sources/Common/Core/DataArray';
 
 import itkreadImageFile from 'itk/readImageFile';
+import Matrix from 'itk/Matrix';
+import getMatrixElement from 'itk/getMatrixElement';
 
 import viewer from './viewer';
 import helper from './helper';
@@ -25,15 +27,22 @@ const processData = (container, { file, use2D }) => {
 
       const dimensions = [1, 1, 1];
 
+      const direction = new Matrix(3, 3);
+      direction.setIdentity();
+
       for (let idx = 0; idx < itkImage.imageType.dimension; ++idx) {
         vtkImage.origin[idx] = itkImage.origin[idx];
         vtkImage.spacing[idx] = itkImage.spacing[idx];
         dimensions[idx] = itkImage.size[idx];
+        for (let col = 0; col < itkImage.imageType.dimension; ++col) {
+          direction.setElement(idx, col, getMatrixElement(itkImage.direction, idx, col));
+        }
       }
 
       // Create VTK Image Data
       const imageData = vtkImageData.newInstance(vtkImage);
       const scalar = vtkDataArray.newInstance(array);
+      imageData.setDirection(direction.data);
       imageData.setDimensions(...dimensions);
       imageData.getPointData().setScalars(scalar);
 

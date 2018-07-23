6189579ea14aade4e6bc3aca2259ad1c170047a5
Matt McCormick <matt.mccormick@kitware.com>
2018-07-23 17:09:36 -0400

    test(createViewerTest): Add baseline image comparison

--- test/createViewerTest.js
+++ test/createViewerTest.js
@@ -10,7 +10,24 @@ import userInterface from '../src/userInterface'
 
 const testImage3DPath = 'base/test/data/input/HeadMRVolume.nrrd'
 
-//import createViewerBaseline from './data/baseline/createViewer.png'
+import createViewerBaseline from './data/baseline/createViewer.png'
+
+const TEST_STYLE_CONTAINER = {
+  position: 'relative',
+  width: '600px',
+  height: '600px',
+  minHeight: '200px',
+  minWidth: '450px',
+  margin: '0',
+  padding: '0',
+  top: '0',
+  left: '0',
+  overflow: 'hidden',
+}
+const TEST_VIEWER_STYLE = {
+  backgroundColor: [1, 1, 1],
+  containerStyle: TEST_STYLE_CONTAINER,
+}
 
 test('Test createViewer', (t) => {
   const container = document.querySelector('body')
@@ -25,13 +42,11 @@ test('Test createViewer', (t) => {
       webWorker.terminate()
 
       const imageData = vtkITKHelper.convertItkToVtkImage(itkImage)
-      const viewer = createViewer(container, { image: imageData })
+      const viewer = createViewer(container, { image: imageData, viewerStyle: TEST_VIEWER_STYLE })
       viewer.captureImage().then((screenshot) => {
-        //testUtils.compareImages(screenshot, [createViewerBaseline], 'Test createViewer', t)
+        testUtils.compareImages(screenshot, [createViewerBaseline], 'Test createViewer', t)
         // clean-up
         userInterface.emptyContainer(container)
-        t.pass()
-        t.end()
       })
       viewer.renderLater()
     })

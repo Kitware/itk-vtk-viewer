a3221f384c046b8e786de853d08bc4c57f993781
Matt McCormick <matt.mccormick@kitware.com>
2018-07-23 23:58:52 -0400

    feat(createViewer): Detect WebGL support
    Per https://developer.mozilla.org/en-US/docs/Learn/WebGL/By_example/Detect_WebGL

--- src/createViewer.js
+++ src/createViewer.js
@@ -48,6 +48,24 @@ const createViewer = (
   applyStyle(container, config.containerStyle || STYLE_CONTAINER);
   rootContainer.appendChild(container);
 
+  const testCanvas = document.createElement("canvas");
+  const gl = testCanvas.getContext("webgl")
+      || testCanvas.getContext("experimental-webgl");
+  if (!(gl && gl instanceof WebGLRenderingContext)) {
+    const suggestion = document.createElement("p");
+    const preSuggestionText = document.createTextNode("WebGL could not be loaded. ");
+    suggestion.appendChild(preSuggestionText);
+    const getWebGLA = document.createElement("a");
+    getWebGLA.setAttribute("href", "http://get.webgl.org/troubleshooting");
+    const getWebGLAText = document.createTextNode("Try a different browser or video drivers for WebGL support.");
+    getWebGLA.appendChild(getWebGLAText);
+    suggestion.appendChild(getWebGLA);
+    const suggestionText = document.createTextNode(" This is required to view interactive 3D visualizations.");
+    suggestion.appendChild(suggestionText);
+    container.appendChild(suggestion);
+    return null;
+  }
+
   const view = proxyManager.createProxy('Views', 'ItkVtkView');
   view.setContainer(container);
   view.setBackground(config.backgroundColor);

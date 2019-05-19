cf838cfce2a749f56e09977bba870ce2b3be130f
Matt McCormick <matt.mccormick@kitware.com>
2019-05-19 12:36:39 -0400

    refactor(GeometriesUI): Factor createGeometryRepresentationSelector

--- src/UserInterface/createGeometriesUI.js
+++ src/UserInterface/createGeometriesUI.js
@@ -3,42 +3,7 @@ import style from './ItkVtkViewer.module.css';
 
 import opacityIcon from './icons/opacity.svg';
 
-function createGeometryRepresentationSelector(
-  viewerDOMId,
-  geometryNames,
-  renderWindow,
-  geometryRepresentationProxies,
-  geometrySelector,
-  geometryRepresentationRow
-) {
-  const geometryRepresentations = new Array(geometryNames.length)
-  geometryRepresentations.fill('Surface')
-  const geometryRepresentationOptions = ['Hidden', 'Wireframe', 'Surface', 'Surface with edges']
-  const geometryRepresentationSelector = document.createElement('select');
-  geometryRepresentationSelector.setAttribute('class', style.selector);
-  geometryRepresentationSelector.id = `${viewerDOMId}-geometryRepresentationSelector`;
-  geometryRepresentationSelector.innerHTML = geometryRepresentationOptions
-    .map((name, idx) => `<option value="${name}">${name}</option>`)
-    .join('')
-  geometryRepresentationSelector.value = 'Surface'
-  geometryRepresentationRow.appendChild(geometryRepresentationSelector)
-  geometrySelector.addEventListener('change',
-    (event) => {
-      geometryRepresentationSelector.value = geometryRepresentations[geometrySelector.selectedIndex]
-    })
-  geometryRepresentationSelector.addEventListener('change',
-    (event) => {
-      const value = event.target.value
-      if(value === 'Hidden') {
-        geometryRepresentationProxies[geometrySelector.selectedIndex].setVisibility(false)
-      } else {
-        geometryRepresentationProxies[geometrySelector.selectedIndex].setRepresentation(value)
-        geometryRepresentationProxies[geometrySelector.selectedIndex].setVisibility(true)
-      }
-      renderWindow.render()
-      geometryRepresentations[geometrySelector.selectedIndex] = value
-    })
-}
+import createGeometryRepresentationSelector from './Geometries/createGeometryRepresentationSelector';
 
 function createGeometryColorChooser(
   viewerDOMId,

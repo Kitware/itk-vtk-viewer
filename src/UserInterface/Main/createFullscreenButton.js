ae045cff72856f53a0a134ff13c6c99e43e4735f
Matt McCormick <matt.mccormick@kitware.com>
2019-05-19 11:43:38 -0400

    refactor(MainUI): Factor createFullscreenButton

---
+++ src/UserInterface/Main/createFullscreenButton.js
@@ -0,0 +1,65 @@
+import style from '../ItkVtkViewer.module.css';
+
+import fullscreenIcon from '../icons/fullscreen.svg';
+
+function createFullscreenButton(
+  viewerDOMId,
+  contrastSensitiveStyle,
+  rootContainer,
+  mainUIRow
+) {
+  const body = document.querySelector('body');
+  let fullScreenMethods = null;
+  // https://developer.mozilla.org/en-US/docs/Web/API/Fullscreen_API
+  [
+    ['requestFullscreen', 'exitFullscreen', 'fullscreenchange', 'fullscreen'],
+    ['mozRequestFullScreen', 'mozCancelFullScreen', 'mozfullscreenchange', 'mozFullScreen'],
+    ['msRequestFullscreen', 'msExitFullscreen', 'MSFullscreenChange', 'msFullscreenEnabled'],
+    ['webkitRequestFullscreen', 'webkitExitFullscreen', 'webkitfullscreenchange', 'webkitIsFullScreen'],
+  ].forEach((methods) => {
+    if (body[methods[0]] && !fullScreenMethods) {
+      fullScreenMethods = methods;
+    }
+  });
+
+  if (fullScreenMethods) {
+    const fullscreenButton = document.createElement('div');
+    fullscreenButton.innerHTML = `<input id="${viewerDOMId}-toggleFullscreenButton" type="checkbox" class="${
+      style.toggleInput
+    }"><label itk-vtk-tooltip itk-vtk-tooltip-top-annotation itk-vtk-tooltip-content="Fullscreen[f]" class="${
+      contrastSensitiveStyle.invertibleButton
+    } ${style.fullscreenButton} ${
+      style.toggleButton
+    }" for="${viewerDOMId}-toggleFullscreenButton">${fullscreenIcon}</label>`;
+    const fullscreenButtonInput = fullscreenButton.children[0];
+    const container = rootContainer.children[0];
+    const oldWidth = container.style.width;
+    const oldHeight = container.style.height;
+    function toggleFullscreen() {
+      const fullscreenEnabled = fullscreenButtonInput.checked;
+      if (fullscreenEnabled) {
+        container.style.width = '100vw';
+        container.style.height = '100vh';
+        rootContainer[fullScreenMethods[0]]();
+      } else {
+        container.style.width = oldWidth;
+        container.style.height = oldHeight;
+        document[fullScreenMethods[1]]();
+      }
+    }
+    fullscreenButton.addEventListener('change', (event) => {
+      toggleFullscreen();
+    });
+    document.addEventListener(fullScreenMethods[2], (event) => {
+      if (!document[fullScreenMethods[3]]) {
+        container.style.width = oldWidth;
+        container.style.height = oldHeight;
+        fullscreenButtonInput.checked = false;
+      }
+    })
+    mainUIRow.appendChild(fullscreenButton);
+  }
+}
+
+export default createFullscreenButton;
+
--- src/UserInterface/createMainUI.js
+++ src/UserInterface/createMainUI.js
@@ -5,13 +5,11 @@ import getContrastSensitiveStyle from './getContrastSensitiveStyle';
 
 import style from './ItkVtkViewer.module.css';
 
-import screenshotIcon from './icons/screenshot.svg';
 import volumeRenderingIcon from './icons/volume-rendering.svg';
 import xPlaneIcon from './icons/x-plane.svg';
 import yPlaneIcon from './icons/y-plane.svg';
 import zPlaneIcon from './icons/z-plane.svg';
 import annotationIcon from './icons/annotations.svg';
-import fullscreenIcon from './icons/fullscreen.svg';
 import interpolationIcon from './icons/interpolation.svg';
 import cropIcon from './icons/crop.svg';
 import resetCropIcon from './icons/reset-crop.svg';
@@ -19,6 +17,7 @@ import resetCameraIcon from './icons/reset-camera.svg';
 
 import createToggleUserInterface from './Main/createToggleUserInterfaceButton';
 import createScreenshotButton from './Main/createScreenshotButton';
+import createFullscreenButton from './Main/createFullscreenButton';
 
 function createMainUI(
   rootContainer,
@@ -59,58 +58,12 @@ function createMainUI(
     mainUIRow
   )
 
-  const body = document.querySelector('body');
-  let fullScreenMethods = null;
-  // https://developer.mozilla.org/en-US/docs/Web/API/Fullscreen_API
-  [
-    ['requestFullscreen', 'exitFullscreen', 'fullscreenchange', 'fullscreen'],
-    ['mozRequestFullScreen', 'mozCancelFullScreen', 'mozfullscreenchange', 'mozFullScreen'],
-    ['msRequestFullscreen', 'msExitFullscreen', 'MSFullscreenChange', 'msFullscreenEnabled'],
-    ['webkitRequestFullscreen', 'webkitExitFullscreen', 'webkitfullscreenchange', 'webkitIsFullScreen'],
-  ].forEach((methods) => {
-    if (body[methods[0]] && !fullScreenMethods) {
-      fullScreenMethods = methods;
-    }
-  });
-
-  if (fullScreenMethods) {
-    const fullscreenButton = document.createElement('div');
-    fullscreenButton.innerHTML = `<input id="${viewerDOMId}-toggleFullscreenButton" type="checkbox" class="${
-      style.toggleInput
-    }"><label itk-vtk-tooltip itk-vtk-tooltip-top-annotation itk-vtk-tooltip-content="Fullscreen[f]" class="${
-      contrastSensitiveStyle.invertibleButton
-    } ${style.fullscreenButton} ${
-      style.toggleButton
-    }" for="${viewerDOMId}-toggleFullscreenButton">${fullscreenIcon}</label>`;
-    const fullscreenButtonInput = fullscreenButton.children[0];
-    const container = rootContainer.children[0];
-    const oldWidth = container.style.width;
-    const oldHeight = container.style.height;
-    function toggleFullscreen() {
-      const fullscreenEnabled = fullscreenButtonInput.checked;
-      if (fullscreenEnabled) {
-        container.style.width = '100vw';
-        container.style.height = '100vh';
-        rootContainer[fullScreenMethods[0]]();
-      } else {
-        container.style.width = oldWidth;
-        container.style.height = oldHeight;
-        document[fullScreenMethods[1]]();
-      }
-    }
-    fullscreenButton.addEventListener('change', (event) => {
-      toggleFullscreen();
-    });
-    document.addEventListener(fullScreenMethods[2], (event) => {
-      if (!document[fullScreenMethods[3]]) {
-        container.style.width = oldWidth;
-        container.style.height = oldHeight;
-        fullscreenButtonInput.checked = false;
-      }
-    })
-    mainUIRow.appendChild(fullscreenButton);
-  }
-
+  createFullscreenButton(
+    viewerDOMId,
+    contrastSensitiveStyle,
+    rootContainer,
+    mainUIRow
+  )
 
   const annotationButton = document.createElement('div');
   annotationButton.innerHTML = `<input id="${viewerDOMId}-toggleAnnotationsButton" type="checkbox" class="${

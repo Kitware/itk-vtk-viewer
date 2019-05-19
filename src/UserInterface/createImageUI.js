d4bff84cc146d57bd3b5ba1365484201e5f5f057
Matt McCormick <matt.mccormick@kitware.com>
2019-05-19 12:15:11 -0400

    refactor(ImageUI): Factor createViewPlanesToggle

--- src/UserInterface/createImageUI.js
+++ src/UserInterface/createImageUI.js
@@ -10,53 +10,8 @@ import style from './ItkVtkViewer.module.css';
 
 import shadowIcon from './icons/shadow.svg';
 import gradientOpacityIcon from './icons/gradient.svg';
-import viewPlansIcon from './icons/view-planes.svg';
 
-function createViewPlanesToggle(
-  imageUIGroup,
-  viewerDOMId,
-  volumeRenderingRow,
-  view,
-  isBackgroundDark
-) {
-  const contrastSensitiveStyle = getContrastSensitiveStyle(
-    ['tooltipButton'],
-    isBackgroundDark
-  );
-
-  let viewPlanes = false;
-  function setViewPlanes() {
-    viewPlanes = !viewPlanes;
-    view.setViewPlanes(viewPlanes);
-    const xPlaneRow = imageUIGroup.querySelector(`.${viewerDOMId}-x-plane-row`);
-    const yPlaneRow = imageUIGroup.querySelector(`.${viewerDOMId}-y-plane-row`);
-    const zPlaneRow = imageUIGroup.querySelector(`.${viewerDOMId}-z-plane-row`);
-    if (view.getViewMode() === 'VolumeRendering') {
-      if (viewPlanes) {
-        xPlaneRow.style.display = 'flex';
-        yPlaneRow.style.display = 'flex';
-        zPlaneRow.style.display = 'flex';
-      } else {
-        xPlaneRow.style.display = 'none';
-        yPlaneRow.style.display = 'none';
-        zPlaneRow.style.display = 'none';
-      }
-    }
-  }
-
-  const viewPlanesButton = document.createElement('div');
-  viewPlanesButton.innerHTML = `<input id="${viewerDOMId}-toggleSlicingPlanesButton" type="checkbox" class="${
-    style.toggleInput
-  }"><label itk-vtk-tooltip itk-vtk-tooltip-top-annotation itk-vtk-tooltip-content="View planes [s]" class="${
-    contrastSensitiveStyle.tooltipButton
-  } ${style.viewPlanesButton} ${
-    style.toggleButton
-  }" for="${viewerDOMId}-toggleSlicingPlanesButton">${viewPlansIcon}</label>`;
-  viewPlanesButton.addEventListener('change', (event) => {
-    setViewPlanes();
-  });
-  volumeRenderingRow.appendChild(viewPlanesButton);
-}
+import createViewPlanesToggle from './Image/createViewPlanesToggle';
 
 function createUseShadowToggle(
   uiContainer,

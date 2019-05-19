81c0313ef1970f38186f62773cbba4f9e241430d
Matt McCormick <matt.mccormick@kitware.com>
2019-05-19 12:45:41 -0400

    refactor(GeometriesUI): Factor createGeometriesOpacitySlider

---
+++ src/UserInterface/Geometries/createGeometryOpacitySlider.js
@@ -0,0 +1,52 @@
+import getContrastSensitiveStyle from '../getContrastSensitiveStyle';
+
+import style from '../ItkVtkViewer.module.css';
+
+import opacityIcon from '../icons/opacity.svg';
+
+function createGeometryOpacitySlider(
+  viewerDOMId,
+  geometryNames,
+  renderWindow,
+  geometryRepresentationProxies,
+  isBackgroundDark,
+  geometrySelector,
+  geometryColorRow
+) {
+  const contrastSensitiveStyle = getContrastSensitiveStyle(
+    ['invertibleButton'],
+    isBackgroundDark
+  );
+  const geometryOpacities = new Array(geometryNames.length);
+  const defaultGeometryOpacity = 1.0;
+  geometryOpacities.fill(defaultGeometryOpacity);
+  const sliderEntry = document.createElement('div');
+  sliderEntry.setAttribute('class', style.sliderEntry);
+  sliderEntry.innerHTML = `
+    <div itk-vtk-tooltip itk-vtk-tooltip-top itk-vtk-tooltip-content="Opacity" class="${
+      contrastSensitiveStyle.invertibleButton
+    } ${style.gradientOpacitySlider}">
+      ${opacityIcon}
+    </div>
+    <input type="range" min="0" max="1" value="${defaultGeometryOpacity}" step="0.01"
+      id="${viewerDOMId}-opacitySlider"
+      class="${style.slider}" />`;
+  const opacityElement = sliderEntry.querySelector(
+    `#${viewerDOMId}-opacitySlider`
+  );
+  function updateOpacity() {
+    const value = Number(opacityElement.value);
+    geometryOpacities[geometrySelector.selectedIndex] = value
+    geometryRepresentationProxies[geometrySelector.selectedIndex].setOpacity(value)
+    renderWindow.render();
+  }
+  opacityElement.addEventListener('input', updateOpacity);
+  updateOpacity();
+  geometrySelector.addEventListener('change',
+    (event) => {
+      opacityElement.value = geometryOpacities[geometrySelector.selectedIndex]
+    });
+  geometryColorRow.appendChild(sliderEntry);
+}
+
+export default createGeometryOpacitySlider;
--- src/UserInterface/createGeometriesUI.js
+++ src/UserInterface/createGeometriesUI.js
@@ -1,54 +1,8 @@
-import getContrastSensitiveStyle from './getContrastSensitiveStyle';
 import style from './ItkVtkViewer.module.css';
-
-import opacityIcon from './icons/opacity.svg';
 
 import createGeometryRepresentationSelector from './Geometries/createGeometryRepresentationSelector';
 import createGeometryColorChooser from './Geometries/createGeometryColorChooser';
-
-function createGeometryOpacitySlider(
-  viewerDOMId,
-  geometryNames,
-  renderWindow,
-  geometryRepresentationProxies,
-  isBackgroundDark,
-  geometrySelector,
-  geometryColorRow
-) {
-  const contrastSensitiveStyle = getContrastSensitiveStyle(
-    ['invertibleButton'],
-    isBackgroundDark
-  );
-  const geometryOpacities = new Array(geometryNames.length)
-  geometryOpacities.fill(0.7)
-  const sliderEntry = document.createElement('div');
-  sliderEntry.setAttribute('class', style.sliderEntry);
-  sliderEntry.innerHTML = `
-    <div itk-vtk-tooltip itk-vtk-tooltip-top itk-vtk-tooltip-content="Opacity" class="${
-      contrastSensitiveStyle.invertibleButton
-    } ${style.gradientOpacitySlider}">
-      ${opacityIcon}
-    </div>
-    <input type="range" min="0" max="1" value="0.7" step="0.01"
-      id="${viewerDOMId}-opacitySlider"
-      class="${style.slider}" />`;
-  const opacityElement = sliderEntry.querySelector(
-    `#${viewerDOMId}-opacitySlider`
-  );
-  function updateOpacity() {
-    const value = Number(opacityElement.value);
-    geometryOpacities[geometrySelector.selectedIndex] = value
-    geometryRepresentationProxies[geometrySelector.selectedIndex].setOpacity(value)
-    renderWindow.render();
-  }
-  opacityElement.addEventListener('input', updateOpacity);
-  updateOpacity();
-  geometrySelector.addEventListener('change',
-    (event) => {
-      opacityElement.value = geometryOpacities[geometrySelector.selectedIndex]
-    })
-  geometryColorRow.appendChild(sliderEntry);
-}
+import createGeometryOpacitySlider from './Geometries/createGeometryOpacitySlider';
 
 function createGeometriesUI(
   uiContainer,

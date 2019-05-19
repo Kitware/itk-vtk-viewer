e20dd9fc144f2ac9e793edcf98fafa0eee92eecb
Matt McCormick <matt.mccormick@kitware.com>
2019-05-19 11:58:09 -0400

    refactor(MainUI): Factor createViewModeButtons

---
+++ src/UserInterface/Main/createViewModeButtons.js
@@ -0,0 +1,157 @@
+import style from '../ItkVtkViewer.module.css';
+
+import volumeRenderingIcon from '../icons/volume-rendering.svg';
+import xPlaneIcon from '../icons/x-plane.svg';
+import yPlaneIcon from '../icons/y-plane.svg';
+import zPlaneIcon from '../icons/z-plane.svg';
+
+function createViewModeButtons(
+  viewerDOMId,
+  contrastSensitiveStyle,
+  view,
+  imageRepresentationProxy,
+  uiContainer,
+  use2D,
+  mainUIRow
+) {
+  function setViewModeXPlane() {
+    view.setViewMode('XPlane');
+    document.getElementById(`${viewerDOMId}-xPlaneButton`).checked = true;
+    document.getElementById(`${viewerDOMId}-yPlaneButton`).checked = false;
+    document.getElementById(`${viewerDOMId}-zPlaneButton`).checked = false;
+    document.getElementById(
+      `${viewerDOMId}-volumeRenderingButton`
+    ).checked = false;
+    if (imageRepresentationProxy) {
+      const volumeRenderingRow = uiContainer.querySelector(
+        `.${viewerDOMId}-volumeRendering`
+      );
+      volumeRenderingRow.style.display = 'none';
+      const xPlaneRow = uiContainer.querySelector(`.${viewerDOMId}-x-plane-row`);
+      xPlaneRow.style.display = 'flex';
+      const yPlaneRow = uiContainer.querySelector(`.${viewerDOMId}-y-plane-row`);
+      yPlaneRow.style.display = 'none';
+      const zPlaneRow = uiContainer.querySelector(`.${viewerDOMId}-z-plane-row`);
+      zPlaneRow.style.display = 'none';
+    }
+  }
+  function setViewModeYPlane() {
+    view.setViewMode('YPlane');
+    document.getElementById(`${viewerDOMId}-xPlaneButton`).checked = false;
+    document.getElementById(`${viewerDOMId}-yPlaneButton`).checked = true;
+    document.getElementById(`${viewerDOMId}-zPlaneButton`).checked = false;
+    document.getElementById(
+      `${viewerDOMId}-volumeRenderingButton`
+    ).checked = false;
+    if (imageRepresentationProxy) {
+      const volumeRenderingRow = uiContainer.querySelector(
+        `.${viewerDOMId}-volumeRendering`
+      );
+      volumeRenderingRow.style.display = 'none';
+      const xPlaneRow = uiContainer.querySelector(`.${viewerDOMId}-x-plane-row`);
+      xPlaneRow.style.display = 'none';
+      const yPlaneRow = uiContainer.querySelector(`.${viewerDOMId}-y-plane-row`);
+      yPlaneRow.style.display = 'flex';
+      const zPlaneRow = uiContainer.querySelector(`.${viewerDOMId}-z-plane-row`);
+      zPlaneRow.style.display = 'none';
+    }
+  }
+  function setViewModeZPlane() {
+    view.setViewMode('ZPlane');
+    document.getElementById(`${viewerDOMId}-xPlaneButton`).checked = false;
+    document.getElementById(`${viewerDOMId}-yPlaneButton`).checked = false;
+    document.getElementById(`${viewerDOMId}-zPlaneButton`).checked = true;
+    document.getElementById(
+      `${viewerDOMId}-volumeRenderingButton`
+    ).checked = false;
+    if (imageRepresentationProxy) {
+      const volumeRenderingRow = uiContainer.querySelector(
+        `.${viewerDOMId}-volumeRendering`
+      );
+      volumeRenderingRow.style.display = 'none';
+      const xPlaneRow = uiContainer.querySelector(`.${viewerDOMId}-x-plane-row`);
+      xPlaneRow.style.display = 'none';
+      const yPlaneRow = uiContainer.querySelector(`.${viewerDOMId}-y-plane-row`);
+      yPlaneRow.style.display = 'none';
+      const zPlaneRow = uiContainer.querySelector(`.${viewerDOMId}-z-plane-row`);
+      zPlaneRow.style.display = 'flex';
+    }
+  }
+  function setViewModeVolumeRendering() {
+    view.setViewMode('VolumeRendering');
+    document.getElementById(`${viewerDOMId}-xPlaneButton`).checked = false;
+    document.getElementById(`${viewerDOMId}-yPlaneButton`).checked = false;
+    document.getElementById(`${viewerDOMId}-zPlaneButton`).checked = false;
+    document.getElementById(
+      `${viewerDOMId}-volumeRenderingButton`
+    ).checked = true;
+    if (imageRepresentationProxy) {
+      const volumeRenderingRow = uiContainer.querySelector(
+        `.${viewerDOMId}-volumeRendering`
+      );
+      volumeRenderingRow.style.display = 'flex';
+      const viewPlanes = document.getElementById(
+        `${viewerDOMId}-toggleSlicingPlanesButton`
+      ).checked;
+      const xPlaneRow = uiContainer.querySelector(`.${viewerDOMId}-x-plane-row`);
+      const yPlaneRow = uiContainer.querySelector(`.${viewerDOMId}-y-plane-row`);
+      const zPlaneRow = uiContainer.querySelector(`.${viewerDOMId}-z-plane-row`);
+      if (viewPlanes) {
+        xPlaneRow.style.display = 'flex';
+        yPlaneRow.style.display = 'flex';
+        zPlaneRow.style.display = 'flex';
+      } else {
+        xPlaneRow.style.display = 'none';
+        yPlaneRow.style.display = 'none';
+        zPlaneRow.style.display = 'none';
+      }
+    }
+  }
+  if (!use2D) {
+    const xPlaneButton = document.createElement('div');
+    xPlaneButton.innerHTML = `<input id="${viewerDOMId}-xPlaneButton" type="checkbox" class="${
+      style.toggleInput
+    }"><label itk-vtk-tooltip itk-vtk-tooltip-top itk-vtk-tooltip-content="X plane [1]" class="${
+      contrastSensitiveStyle.tooltipButton
+    } ${style.viewModeButton} ${
+      style.toggleButton
+    }" for="${viewerDOMId}-xPlaneButton">${xPlaneIcon}</label>`;
+    xPlaneButton.addEventListener('click', setViewModeXPlane);
+    mainUIRow.appendChild(xPlaneButton);
+
+    const yPlaneButton = document.createElement('div');
+    yPlaneButton.innerHTML = `<input id="${viewerDOMId}-yPlaneButton" type="checkbox" class="${
+      style.toggleInput
+    }"><label itk-vtk-tooltip itk-vtk-tooltip-top itk-vtk-tooltip-content="Y plane [2]" class="${
+      contrastSensitiveStyle.tooltipButton
+    } ${style.viewModeButton} ${
+      style.toggleButton
+    }" for="${viewerDOMId}-yPlaneButton">${yPlaneIcon}</label>`;
+    yPlaneButton.addEventListener('click', setViewModeYPlane);
+    mainUIRow.appendChild(yPlaneButton);
+
+    const zPlaneButton = document.createElement('div');
+    zPlaneButton.innerHTML = `<input id="${viewerDOMId}-zPlaneButton" type="checkbox" class="${
+      style.toggleInput
+    }"><label itk-vtk-tooltip itk-vtk-tooltip-top itk-vtk-tooltip-content="Z plane [3]" class="${
+      contrastSensitiveStyle.tooltipButton
+    } ${style.viewModeButton} ${
+      style.toggleButton
+    }" for="${viewerDOMId}-zPlaneButton">${zPlaneIcon}</label>`;
+    zPlaneButton.addEventListener('click', setViewModeZPlane);
+    mainUIRow.appendChild(zPlaneButton);
+
+    const volumeRenderingButton = document.createElement('div');
+    volumeRenderingButton.innerHTML = `<input id="${viewerDOMId}-volumeRenderingButton" type="checkbox" class="${
+      style.toggleInput
+    }" checked><label itk-vtk-tooltip itk-vtk-tooltip-top itk-vtk-tooltip-content="Volume [4]" class="${
+      contrastSensitiveStyle.tooltipButton
+    } ${style.viewModeButton} ${
+      style.toggleButton
+    }" for="${viewerDOMId}-volumeRenderingButton">${volumeRenderingIcon}</label>`;
+    volumeRenderingButton.addEventListener('click', setViewModeVolumeRendering);
+    mainUIRow.appendChild(volumeRenderingButton);
+  }
+}
+
+export default createViewModeButtons;
--- src/UserInterface/createMainUI.js
+++ src/UserInterface/createMainUI.js
@@ -9,7 +9,6 @@ import volumeRenderingIcon from './icons/volume-rendering.svg';
 import xPlaneIcon from './icons/x-plane.svg';
 import yPlaneIcon from './icons/y-plane.svg';
 import zPlaneIcon from './icons/z-plane.svg';
-import interpolationIcon from './icons/interpolation.svg';
 import cropIcon from './icons/crop.svg';
 import resetCropIcon from './icons/reset-crop.svg';
 import resetCameraIcon from './icons/reset-camera.svg';
@@ -19,6 +18,7 @@ import createScreenshotButton from './Main/createScreenshotButton';
 import createFullscreenButton from './Main/createFullscreenButton';
 import createAnnotationButton from './Main/createAnnotationButton';
 import createInterpolationButton from './Main/createInterpolationButton';
+import createViewModeButtons from './Main/createViewModeButtons';
 
 function createMainUI(
   rootContainer,
@@ -82,144 +82,15 @@ function createMainUI(
     )
   }
 
-  function setViewModeXPlane() {
-    view.setViewMode('XPlane');
-    document.getElementById(`${viewerDOMId}-xPlaneButton`).checked = true;
-    document.getElementById(`${viewerDOMId}-yPlaneButton`).checked = false;
-    document.getElementById(`${viewerDOMId}-zPlaneButton`).checked = false;
-    document.getElementById(
-      `${viewerDOMId}-volumeRenderingButton`
-    ).checked = false;
-    if (imageRepresentationProxy) {
-      const volumeRenderingRow = uiContainer.querySelector(
-        `.${viewerDOMId}-volumeRendering`
-      );
-      volumeRenderingRow.style.display = 'none';
-      const xPlaneRow = uiContainer.querySelector(`.${viewerDOMId}-x-plane-row`);
-      xPlaneRow.style.display = 'flex';
-      const yPlaneRow = uiContainer.querySelector(`.${viewerDOMId}-y-plane-row`);
-      yPlaneRow.style.display = 'none';
-      const zPlaneRow = uiContainer.querySelector(`.${viewerDOMId}-z-plane-row`);
-      zPlaneRow.style.display = 'none';
-    }
-  }
-  function setViewModeYPlane() {
-    view.setViewMode('YPlane');
-    document.getElementById(`${viewerDOMId}-xPlaneButton`).checked = false;
-    document.getElementById(`${viewerDOMId}-yPlaneButton`).checked = true;
-    document.getElementById(`${viewerDOMId}-zPlaneButton`).checked = false;
-    document.getElementById(
-      `${viewerDOMId}-volumeRenderingButton`
-    ).checked = false;
-    if (imageRepresentationProxy) {
-      const volumeRenderingRow = uiContainer.querySelector(
-        `.${viewerDOMId}-volumeRendering`
-      );
-      volumeRenderingRow.style.display = 'none';
-      const xPlaneRow = uiContainer.querySelector(`.${viewerDOMId}-x-plane-row`);
-      xPlaneRow.style.display = 'none';
-      const yPlaneRow = uiContainer.querySelector(`.${viewerDOMId}-y-plane-row`);
-      yPlaneRow.style.display = 'flex';
-      const zPlaneRow = uiContainer.querySelector(`.${viewerDOMId}-z-plane-row`);
-      zPlaneRow.style.display = 'none';
-    }
-  }
-  function setViewModeZPlane() {
-    view.setViewMode('ZPlane');
-    document.getElementById(`${viewerDOMId}-xPlaneButton`).checked = false;
-    document.getElementById(`${viewerDOMId}-yPlaneButton`).checked = false;
-    document.getElementById(`${viewerDOMId}-zPlaneButton`).checked = true;
-    document.getElementById(
-      `${viewerDOMId}-volumeRenderingButton`
-    ).checked = false;
-    if (imageRepresentationProxy) {
-      const volumeRenderingRow = uiContainer.querySelector(
-        `.${viewerDOMId}-volumeRendering`
-      );
-      volumeRenderingRow.style.display = 'none';
-      const xPlaneRow = uiContainer.querySelector(`.${viewerDOMId}-x-plane-row`);
-      xPlaneRow.style.display = 'none';
-      const yPlaneRow = uiContainer.querySelector(`.${viewerDOMId}-y-plane-row`);
-      yPlaneRow.style.display = 'none';
-      const zPlaneRow = uiContainer.querySelector(`.${viewerDOMId}-z-plane-row`);
-      zPlaneRow.style.display = 'flex';
-    }
-  }
-  function setViewModeVolumeRendering() {
-    view.setViewMode('VolumeRendering');
-    document.getElementById(`${viewerDOMId}-xPlaneButton`).checked = false;
-    document.getElementById(`${viewerDOMId}-yPlaneButton`).checked = false;
-    document.getElementById(`${viewerDOMId}-zPlaneButton`).checked = false;
-    document.getElementById(
-      `${viewerDOMId}-volumeRenderingButton`
-    ).checked = true;
-    if (imageRepresentationProxy) {
-      const volumeRenderingRow = uiContainer.querySelector(
-        `.${viewerDOMId}-volumeRendering`
-      );
-      volumeRenderingRow.style.display = 'flex';
-      const viewPlanes = document.getElementById(
-        `${viewerDOMId}-toggleSlicingPlanesButton`
-      ).checked;
-      const xPlaneRow = uiContainer.querySelector(`.${viewerDOMId}-x-plane-row`);
-      const yPlaneRow = uiContainer.querySelector(`.${viewerDOMId}-y-plane-row`);
-      const zPlaneRow = uiContainer.querySelector(`.${viewerDOMId}-z-plane-row`);
-      if (viewPlanes) {
-        xPlaneRow.style.display = 'flex';
-        yPlaneRow.style.display = 'flex';
-        zPlaneRow.style.display = 'flex';
-      } else {
-        xPlaneRow.style.display = 'none';
-        yPlaneRow.style.display = 'none';
-        zPlaneRow.style.display = 'none';
-      }
-    }
-  }
-  if (!use2D) {
-    const xPlaneButton = document.createElement('div');
-    xPlaneButton.innerHTML = `<input id="${viewerDOMId}-xPlaneButton" type="checkbox" class="${
-      style.toggleInput
-    }"><label itk-vtk-tooltip itk-vtk-tooltip-top itk-vtk-tooltip-content="X plane [1]" class="${
-      contrastSensitiveStyle.tooltipButton
-    } ${style.viewModeButton} ${
-      style.toggleButton
-    }" for="${viewerDOMId}-xPlaneButton">${xPlaneIcon}</label>`;
-    xPlaneButton.addEventListener('click', setViewModeXPlane);
-    mainUIRow.appendChild(xPlaneButton);
-
-    const yPlaneButton = document.createElement('div');
-    yPlaneButton.innerHTML = `<input id="${viewerDOMId}-yPlaneButton" type="checkbox" class="${
-      style.toggleInput
-    }"><label itk-vtk-tooltip itk-vtk-tooltip-top itk-vtk-tooltip-content="Y plane [2]" class="${
-      contrastSensitiveStyle.tooltipButton
-    } ${style.viewModeButton} ${
-      style.toggleButton
-    }" for="${viewerDOMId}-yPlaneButton">${yPlaneIcon}</label>`;
-    yPlaneButton.addEventListener('click', setViewModeYPlane);
-    mainUIRow.appendChild(yPlaneButton);
-
-    const zPlaneButton = document.createElement('div');
-    zPlaneButton.innerHTML = `<input id="${viewerDOMId}-zPlaneButton" type="checkbox" class="${
-      style.toggleInput
-    }"><label itk-vtk-tooltip itk-vtk-tooltip-top itk-vtk-tooltip-content="Z plane [3]" class="${
-      contrastSensitiveStyle.tooltipButton
-    } ${style.viewModeButton} ${
-      style.toggleButton
-    }" for="${viewerDOMId}-zPlaneButton">${zPlaneIcon}</label>`;
-    zPlaneButton.addEventListener('click', setViewModeZPlane);
-    mainUIRow.appendChild(zPlaneButton);
-
-    const volumeRenderingButton = document.createElement('div');
-    volumeRenderingButton.innerHTML = `<input id="${viewerDOMId}-volumeRenderingButton" type="checkbox" class="${
-      style.toggleInput
-    }" checked><label itk-vtk-tooltip itk-vtk-tooltip-top itk-vtk-tooltip-content="Volume [4]" class="${
-      contrastSensitiveStyle.tooltipButton
-    } ${style.viewModeButton} ${
-      style.toggleButton
-    }" for="${viewerDOMId}-volumeRenderingButton">${volumeRenderingIcon}</label>`;
-    volumeRenderingButton.addEventListener('click', setViewModeVolumeRendering);
-    mainUIRow.appendChild(volumeRenderingButton);
-  }
+  createViewModeButtons(
+    viewerDOMId,
+    contrastSensitiveStyle,
+    view,
+    imageRepresentationProxy,
+    uiContainer,
+    use2D,
+    mainUIRow
+  )
 
   let croppingWidget = null
   let addCroppingPlanesChangedHandler = () => {}

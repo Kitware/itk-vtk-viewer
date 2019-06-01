981d83164bf9299bebdb3c38b8c9a7e822a7d55f
Matt McCormick <matt.mccormick@kitware.com>
2019-06-01 17:22:52 -0400

    feat(icons): Add point-set-size icon
    From:
    
      https://raw.githubusercontent.com/leungwensen/svg-icon/master/dist/trimmed-svg/awesome/expand.svg

---
+++ src/UserInterface/PointSets/createPointSetSizeSlider.js
@@ -0,0 +1,17 @@
+import getContrastSensitiveStyle from '../getContrastSensitiveStyle';
+
+import style from '../ItkVtkViewer.module.css';
+
+import pointSetSize from '../icons/point-set-size.svg';
+
+function createPointSetSizeSlider(
+  viewerDOMId,
+  renderWindow,
+  pointSetRepresentationProxies,
+  isBackgroundDark,
+  pointSetSelector,
+  pointSetSizeRow
+) {
+}
+
+export default createPointSetSizeSlider;

2d36b103deaa7d38fc05217bc976822bfaf88226
Matt McCormick <matt.mccormick@kitware.com>
2019-11-27 16:20:03 -0500

    refactor(customColorMapIcon): Factor out

--- src/UserInterface/Image/createColorRangeInput.js
+++ src/UserInterface/Image/createColorRangeInput.js
@@ -5,6 +5,7 @@ import vtkLookupTableProxy from 'vtk.js/Sources/Proxy/Core/LookupTableProxy';
 import style from '../ItkVtkViewer.module.css';
 
 import createColorMapIconSelector from '../createColorMapIconSelector';
+import customColorMapIcon from '../customColorMapIcon';
 
 function createColorRangeInput(
   store,
@@ -65,36 +66,6 @@ function createColorRangeInput(
   );
 
   updateColorRangeInput();
-
-  const canvas = document.createElement('canvas');
-  const width = 240;
-  const height = 20;
-  canvas.setAttribute('width', width);
-  canvas.setAttribute('height', height);
-
-  function customColorMapIcon(colorTransferFunction, range) {
-    const ctx = canvas.getContext('2d');
-
-    const rgba = colorTransferFunction.getUint8Table(
-      range[0],
-      range[1],
-      width,
-      4,
-    );
-    const pixelsArea = ctx.getImageData(0, 0, width, 256);
-    for (let lineIdx = 0; lineIdx < 256; lineIdx++) {
-      pixelsArea.data.set(rgba, lineIdx * 4 * width);
-    }
-
-    const nbValues = 256 * width * 4;
-    const lineSize = width * 4;
-    for (let i = 3; i < nbValues; i += 4) {
-      pixelsArea.data[i] = 255 - Math.floor(i / lineSize);
-    }
-
-    ctx.putImageData(pixelsArea, 0, 0);
-    return canvas.toDataURL('image/png')
-  }
 
   const colorMapSelector = document.createElement('div');
   colorMapSelector.id = `${store.id}-imageColorMapSelector`;

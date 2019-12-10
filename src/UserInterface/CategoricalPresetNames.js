291fe863f526296b157df3b09d58f55b27fc55e9
Matt McCormick <matt.mccormick@kitware.com>
2019-12-09 20:22:37 -0500

    feat(CategoricalColors): Initial addition
    From https://github.com/thewtex/vtk-js-colormap-icons

---
+++ src/UserInterface/CategoricalPresetNames.js
@@ -0,0 +1,11 @@
+const CategoricalPresetNames = [
+  'glasbey',
+  'glasbey_light',
+  'glasbey_warm',
+
+  'glasbey_bw',
+  'glasbey_dark',
+  'glasbey_cool',
+];
+
+export default CategoricalPresetNames;

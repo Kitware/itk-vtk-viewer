095c248ebf2e8e5f04e10f6f23bc77eb0cb3b5ba
Travis CI <sebastien.jourdain@kitware.com>
2020-01-28 17:38:17 +0000

    Updates

--- app/serviceWorker.js
+++ app/serviceWorker.js
@@ -1,7 +1,35 @@
-importScripts('workbox-sw.prod.v2.1.2.js');
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
 
-const workboxSW = new self.WorkboxSW({clientsClaim: true})
-workboxSW.precache([
+importScripts("workbox-v4.3.1/workbox-sw.js");
+workbox.setConfig({modulePathPrefix: "workbox-v4.3.1"});
+
+importScripts(
+  "precache-manifest.870d42406272213b6d5be017dabe759a.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [
   {
     "url": "favicon-32x32.png",
     "revision": "7b1da026f1c9dc9d8caadbe4bdb7b2a8"
@@ -11,10 +39,6 @@ workboxSW.precache([
     "revision": "5de1ef0fb1ad69499470ffe1d6e2cf76"
   },
   {
-    "url": "itkVtkViewer.js",
-    "revision": "7ed5976b93a1462337ea8394ef094c49"
-  },
-  {
     "url": "itkVtkViewerCDN.js",
     "revision": "e2617dddfe0d9429a896a218a7bb3b22"
   },
@@ -22,15 +46,7 @@ workboxSW.precache([
     "url": "test.html",
     "revision": "7ae13ed21ff30dda487760df4b24897c"
   }
-])
+].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workboxSW.router.registerRoute(
-  /\.js|\.png|\.wasm$/,
-  workboxSW.strategies.staleWhileRevalidate({
-  cacheName: 'staleWhileRevalidateContent',
-  cacheExpiration: {
-    maxEntries: 50,
-    maxAgeSeconds: 7 * 24 * 60 * 60 * 26,
-    }
-  })
-);
+workbox.routing.registerRoute(/\.js|\.png|\.wasm$/, new workbox.strategies.StaleWhileRevalidate({ "cacheName":"itk-vtk-viewer-StaleWhileRevalidate", plugins: [new workbox.expiration.Plugin({ maxEntries: 50, maxAgeSeconds: 1209600, purgeOnQuotaError: false })] }), 'GET');

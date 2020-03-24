06543b60d6770b2433ee92b169823b6c334f395f
Matt McCormick <matt.mccormick@kitware.com>
2020-03-23 22:56:38 -0400

    refactor(createViewer): Factor checkForWebGL

--- src/createViewer.js
+++ src/createViewer.js
@@ -31,6 +31,9 @@ const createViewer = (
    }
 ) => {
   UserInterface.emptyContainer(rootContainer);
+  if (!UserInterface.checkForWebGL(rootContainer)) {
+    return null;
+  }
 
   const proxyManager = vtkProxyManager.newInstance({ proxyConfiguration });
   window.addEventListener('resize', proxyManager.resizeAllViews);
@@ -39,24 +42,6 @@ const createViewer = (
   const store = new ViewerStore(proxyManager);
 
   UserInterface.applyContainerStyle(rootContainer, store, viewerStyle);
-
-  const testCanvas = document.createElement("canvas");
-  const gl = testCanvas.getContext("webgl")
-      || testCanvas.getContext("experimental-webgl");
-  if (!(gl && gl instanceof WebGLRenderingContext)) {
-    const suggestion = document.createElement("p");
-    const preSuggestionText = document.createTextNode("WebGL could not be loaded. ");
-    suggestion.appendChild(preSuggestionText);
-    const getWebGLA = document.createElement("a");
-    getWebGLA.setAttribute("href", "http://get.webgl.org/troubleshooting");
-    const getWebGLAText = document.createTextNode("Try a different browser or video drivers for WebGL support.");
-    getWebGLA.appendChild(getWebGLAText);
-    suggestion.appendChild(getWebGLA);
-    const suggestionText = document.createTextNode(" This is required to view interactive 3D visualizations.");
-    suggestion.appendChild(suggestionText);
-    store.container.appendChild(suggestion);
-    return null;
-  }
 
   UserInterface.createMainUI(
     rootContainer,

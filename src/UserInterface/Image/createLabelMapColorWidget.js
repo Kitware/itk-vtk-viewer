ca6a3ad6a4283dbf60e7fdc229f3ede36ac2e7fc
Matt McCormick <matt.mccormick@kitware.com>
2020-07-05 21:32:31 -0400

    fix(LabelMapBlendSlider): Reduce debounce time for interactivity

--- src/UserInterface/Image/createLabelMapColorWidget.js
+++ src/UserInterface/Image/createLabelMapColorWidget.js
@@ -113,7 +113,7 @@ function createLabelMapColorWidget(store, uiContainer) {
   }
   reaction(() => {
     return store.imageUI.labelMapBlend
-  }, macro.debounce(updateLabelMapColorOpacity, 25))
+  }, macro.debounce(updateLabelMapColorOpacity, 10))
   updateLabelMapColorOpacity()
   opacityElement.addEventListener(
     'input',

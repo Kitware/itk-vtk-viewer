c7db32e7403d70d8937cd43ff1d4d3c2b37a9186
Matt McCormick <matt.mccormick@kitware.com>
2021-01-27 11:38:55 -0500

    fix(selectImageComponent): Update per-component information

--- src/UI/Reference/Images/selectImageComponent.js
+++ src/UI/Reference/Images/selectImageComponent.js
@@ -1,45 +1,48 @@
 import applyColorRangeBounds from './applyColorRangeBounds'
 import applyColorRange from './applyColorRange'
 import updateRenderedImageInterface from './updateRenderedImageInterface'
+import applyColorMap from './applyColorMap'
 
 function selectImageComponent(context, event) {
   context.images.componentSelector.value = event.data
-  console.log('IMAGE COMPonent', event.data)
 
-  const name = context.images.selectedName
+  const name = event.data.name
   const actorContext = context.images.actorContext.get(name)
+  const component = event.data.component
 
   updateRenderedImageInterface(context, { data: name })
 
-  if (actorContext.colorRanges.has(name)) {
+  if (actorContext.colorRanges.has(component)) {
     applyColorRange(context, {
       data: {
         name,
-        component: actorContext.selectedComponent,
-        range: actorContext.colorRanges.get(name),
+        component,
+        range: actorContext.colorRanges.get(component),
       },
     })
   }
 
-  if (actorContext.colorRangeBounds.has(name)) {
+  if (actorContext.colorRangeBounds.has(component)) {
     applyColorRangeBounds(context, {
       data: {
         name,
-        component: actorContext.selectedComponent,
-        range: actorContext.colorRangeBounds.get(name),
+        component,
+        range: actorContext.colorRangeBounds.get(component),
       },
     })
   }
 
-  if (actorContext.colorMaps.has(name)) {
+  if (actorContext.colorMaps.has(component)) {
     applyColorMap(context, {
       data: {
         name,
-        component: actorContext.selectedComponent,
-        colorMap: actorContext.colorMaps.get(name),
+        component,
+        colorMap: actorContext.colorMaps.get(component),
       },
     })
-    context.images.iconSelector.setSelectedValue(colorMaps)
+    context.images.iconSelector.setSelectedValue(
+      actorContext.colorMaps.get(component)
+    )
   }
 }
 

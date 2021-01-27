386ee8edc76957c86206cc5455ada63300f6cd10
Travis CI <sebastien.jourdain@kitware.com>
2021-01-27 23:19:38 +0000

    Updates

---
+++ app/serviceWorker.js
@@ -0,0 +1,60 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("autoworkbox-v4.3.1/workbox-sw.js");
+workbox.setConfig({modulePathPrefix: "autoworkbox-v4.3.1"});
+
+importScripts(
+  "autoprecache-manifest.870d42406272213b6d5be017dabe759a.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [
+  {
+    "url": "b4ead4d3696006a600b8.jpg",
+    "revision": "e73ecf6eac4e16c51b94aff6ec40ac3f"
+  },
+  {
+    "url": "f15a50b65e584afdbf29.png",
+    "revision": "d607682ba3901d2d5de998c3355079f8"
+  },
+  {
+    "url": "favicon-32x32.png",
+    "revision": "7b1da026f1c9dc9d8caadbe4bdb7b2a8"
+  },
+  {
+    "url": "index.html",
+    "revision": "a2b34729386d363499348b41885886cd"
+  },
+  {
+    "url": "itkVtkViewerCDN.js",
+    "revision": "ae58b79c6f792ed6a505227f926f00c3"
+  },
+  {
+    "url": "test.html",
+    "revision": "7ae13ed21ff30dda487760df4b24897c"
+  }
+].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerRoute(/\.js|\.png|\.wasm$/, new workbox.strategies.StaleWhileRevalidate({ "cacheName":"itk-vtk-viewer-StaleWhileRevalidate", plugins: [new workbox.expiration.Plugin({ maxEntries: 50, maxAgeSeconds: 1209600, purgeOnQuotaError: false })] }), 'GET');

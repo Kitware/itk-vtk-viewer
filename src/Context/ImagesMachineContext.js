98f0f7b402660a9d99c15dee61ec67c798c0d648
Matt McCormick <matt.mccormick@kitware.com>
2021-02-17 15:35:32 -0500

    fix(UI): Select image layer when UI in un-collapsed
    Closes #375

--- src/Context/ImagesMachineContext.js
+++ src/Context/ImagesMachineContext.js
@@ -6,10 +6,10 @@ class ImagesMachineContext {
   actorContext = new Map()
 
   // Name of the selected image
-  selectedName = 'Image'
+  selectedName = null
 
   // Name of the image whose data needs to be updated
-  updateRenderedName = 'Image'
+  updateRenderedName = null
 }
 
 export default ImagesMachineContext

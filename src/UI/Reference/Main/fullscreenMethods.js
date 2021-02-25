0cbcd230de25406da3278bac5e034d55b8130aff
Matt McCormick <matt.mccormick@kitware.com>
2021-02-24 21:35:00 -0500

    fix(fullscreenMethods): Initialize after body has loaded

--- src/UI/Reference/Main/fullscreenMethods.js
+++ src/UI/Reference/Main/fullscreenMethods.js
@@ -1,30 +1,32 @@
-const body = document.querySelector('body')
-let fullscreenMethods = null
-// https://developer.mozilla.org/en-US/docs/Web/API/Fullscreen_API
-;[
-  ['requestFullscreen', 'exitFullscreen', 'fullscreenchange', 'fullscreen'],
-  [
-    'mozRequestFullScreen',
-    'mozCancelFullScreen',
-    'mozfullscreenchange',
-    'mozFullScreen',
-  ],
-  [
-    'msRequestFullscreen',
-    'msExitFullscreen',
-    'MSFullscreenChange',
-    'msFullscreenEnabled',
-  ],
-  [
-    'webkitRequestFullscreen',
-    'webkitExitFullscreen',
-    'webkitfullscreenchange',
-    'webkitIsFullScreen',
-  ],
-].forEach(methods => {
-  if (body[methods[0]] && !fullscreenMethods) {
-    fullscreenMethods = methods
-  }
+const fullscreenMethods = []
+window.addEventListener('load', () => {
+  const body = document.querySelector('body')
+  // https://developer.mozilla.org/en-US/docs/Web/API/Fullscreen_API
+  ;[
+    ['requestFullscreen', 'exitFullscreen', 'fullscreenchange', 'fullscreen'],
+    [
+      'mozRequestFullScreen',
+      'mozCancelFullScreen',
+      'mozfullscreenchange',
+      'mozFullScreen',
+    ],
+    [
+      'msRequestFullscreen',
+      'msExitFullscreen',
+      'MSFullscreenChange',
+      'msFullscreenEnabled',
+    ],
+    [
+      'webkitRequestFullscreen',
+      'webkitExitFullscreen',
+      'webkitfullscreenchange',
+      'webkitIsFullScreen',
+    ],
+  ].forEach(methods => {
+    if (body[methods[0]] && fullscreenMethods.length === 0) {
+      fullscreenMethods.splice(methods, methods.length, ...methods)
+    }
+  })
 })
 
 export default fullscreenMethods

32b7047584b6618d2e255d6833460b65596e5382
Matt McCormick <matt.mccormick@kitware.com>
2021-07-30 18:31:54 -0400

    fix(icons): Commit without linting

--- src/UI/icons/dist/geometry.svg.uri.js
+++ src/UI/icons/dist/geometry.svg.uri.js
@@ -1,4 +1,3 @@
-const optimizedSVGDataUri =
-  "data:image/svg+xml,%3c%3fxml version='1.0' encoding='utf-8'%3f%3e %3c!-- Generator: Adobe Illustrator 23.0.1%2c SVG Export Plug-In . SVG Version: 6.00 Build 0) --%3e %3csvg version='1.1' id='Layer_1' xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' x='0px' y='0px' viewBox='0 0 100 100' style='enable-background:new 0 0 100 100%3b' xml:space='preserve'%3e %3cpolygon points='22.8%2c65.8 29.9%2c18.1 85.7%2c67.4 51.9%2c79.5 '/%3e %3c/svg%3e"
+const optimizedSVGDataUri = "data:image/svg+xml,%3c%3fxml version='1.0' encoding='utf-8'%3f%3e %3c!-- Generator: Adobe Illustrator 23.0.1%2c SVG Export Plug-In . SVG Version: 6.00 Build 0) --%3e %3csvg version='1.1' id='Layer_1' xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' x='0px' y='0px' viewBox='0 0 100 100' style='enable-background:new 0 0 100 100%3b' xml:space='preserve'%3e %3cpolygon points='22.8%2c65.8 29.9%2c18.1 85.7%2c67.4 51.9%2c79.5 '/%3e %3c/svg%3e"
 
 export default optimizedSVGDataUri

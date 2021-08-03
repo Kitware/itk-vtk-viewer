aa8c19f7646f0616b55552e42d3a1e8e18ccfad7
Matt McCormick <matt.mccormick@kitware.com>
2021-08-03 16:52:44 -0400

    fix(reference-ui): Do not depend on the presence of all main buttons

--- src/UI/reference-ui/src/Main/applySlicingPlanes.js
+++ src/UI/reference-ui/src/Main/applySlicingPlanes.js
@@ -1,8 +1,11 @@
 function applySlicingPlanes(context, event) {
   const slicingPlanes = event.data
+  const main = context.main
 
   if (context.use2D) {
-    context.main.viewPlanesButton.style.display = 'none'
+    if (main.viewPlanesButton) {
+      main.viewPlanesButton.style.display = 'none'
+    }
     return
   }
 
@@ -11,66 +14,64 @@ function applySlicingPlanes(context, event) {
     !slicingPlanes.y.visible &&
     !slicingPlanes.z.visible
   ) {
-    context.main.viewPlanesButtonInput.checked = false
+    if (main.viewPlanesButtonInput) {
+      main.viewPlanesButtonInput.checked = false
+    }
   } else {
-    context.main.viewPlanesButtonInput.checked = true
+    if (main.viewPlanesButtonInput) {
+      main.viewPlanesButtonInput.checked = true
+    }
   }
 
-  if (!context.main.planeUIGroup) {
+  if (!main.planeUIGroup) {
     return
   }
 
-  context.main.xSliceElement.min = slicingPlanes.x.min
-  context.main.xSliceElement.max = slicingPlanes.x.max
-  context.main.xSliceElement.step = slicingPlanes.x.step
+  main.xSliceElement.min = slicingPlanes.x.min
+  main.xSliceElement.max = slicingPlanes.x.max
+  main.xSliceElement.step = slicingPlanes.x.step
 
-  context.main.ySliceElement.min = slicingPlanes.y.min
-  context.main.ySliceElement.max = slicingPlanes.y.max
-  context.main.ySliceElement.step = slicingPlanes.y.step
+  main.ySliceElement.min = slicingPlanes.y.min
+  main.ySliceElement.max = slicingPlanes.y.max
+  main.ySliceElement.step = slicingPlanes.y.step
 
-  context.main.zSliceElement.min = slicingPlanes.z.min
-  context.main.zSliceElement.max = slicingPlanes.z.max
-  context.main.zSliceElement.step = slicingPlanes.z.step
+  main.zSliceElement.min = slicingPlanes.z.min
+  main.zSliceElement.max = slicingPlanes.z.max
+  main.zSliceElement.step = slicingPlanes.z.step
 
-  if (context.main.viewMode === 'Volume') {
-    context.main.xPlaneVisibleButton.style.display = slicingPlanes.x.visible
+  if (main.viewMode === 'Volume') {
+    main.xPlaneVisibleButton.style.display = slicingPlanes.x.visible
       ? 'flex'
       : 'none'
-    context.main.xPlaneInvisibleButton.style.display = slicingPlanes.x.visible
+    main.xPlaneInvisibleButton.style.display = slicingPlanes.x.visible
       ? 'none'
       : 'flex'
-    context.main.yPlaneVisibleButton.style.display = slicingPlanes.y.visible
+    main.yPlaneVisibleButton.style.display = slicingPlanes.y.visible
       ? 'flex'
       : 'none'
-    context.main.yPlaneInvisibleButton.style.display = slicingPlanes.y.visible
+    main.yPlaneInvisibleButton.style.display = slicingPlanes.y.visible
       ? 'none'
       : 'flex'
-    context.main.zPlaneVisibleButton.style.display = slicingPlanes.z.visible
+    main.zPlaneVisibleButton.style.display = slicingPlanes.z.visible
       ? 'flex'
       : 'none'
-    context.main.zPlaneInvisibleButton.style.display = slicingPlanes.z.visible
+    main.zPlaneInvisibleButton.style.display = slicingPlanes.z.visible
       ? 'none'
       : 'flex'
   }
 
-  context.main.xPlanePauseButton.style.display = slicingPlanes.x.scroll
+  main.xPlanePauseButton.style.display = slicingPlanes.x.scroll
     ? 'flex'
     : 'none'
-  context.main.xPlanePlayButton.style.display = slicingPlanes.x.scroll
-    ? 'none'
-    : 'flex'
-  context.main.yPlanePauseButton.style.display = slicingPlanes.y.scroll
+  main.xPlanePlayButton.style.display = slicingPlanes.x.scroll ? 'none' : 'flex'
+  main.yPlanePauseButton.style.display = slicingPlanes.y.scroll
     ? 'flex'
     : 'none'
-  context.main.yPlanePlayButton.style.display = slicingPlanes.y.scroll
-    ? 'none'
-    : 'flex'
-  context.main.zPlanePauseButton.style.display = slicingPlanes.z.scroll
+  main.yPlanePlayButton.style.display = slicingPlanes.y.scroll ? 'none' : 'flex'
+  main.zPlanePauseButton.style.display = slicingPlanes.z.scroll
     ? 'flex'
     : 'none'
-  context.main.zPlanePlayButton.style.display = slicingPlanes.z.scroll
-    ? 'none'
-    : 'flex'
+  main.zPlanePlayButton.style.display = slicingPlanes.z.scroll ? 'none' : 'flex'
 }
 
 export default applySlicingPlanes

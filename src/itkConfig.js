d160117d33f2eeb3031a761f47939c8b747540b8
Paul Elliott <paul.elliott@kitware.com>
2022-05-10 10:02:05 -0400

    fix(types): export as UMD and aruments to viewer::setBackgroundColor

--- src/itkConfig.js
+++ src/itkConfig.js
@@ -1,8 +1,8 @@
 const itkConfig = {
-  pipelineWorkerUrl: 'pipeline.worker.js', // eslint-disable-line no-undef
-  imageIOUrl: 'itk/image-io', // eslint-disable-line no-undef
-  meshIOUrl: 'itk/mesh-io', // eslint-disable-line no-undef
-  pipelinesUrl: 'itk/pipeline', // eslint-disable-line no-undef
+  pipelineWorkerUrl: 'pipeline.worker.js',
+  imageIOUrl: 'itk/image-io',
+  meshIOUrl: 'itk/mesh-io',
+  pipelinesUrl: 'itk/pipeline',
 }
 
 export default itkConfig

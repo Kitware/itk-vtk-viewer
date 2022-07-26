25abb499abb918561e42294a99eb3748a135e398
Laryssa Abdala <la.abdala@gmail.com>
2022-07-26 11:43:54 -0400

    feat(create-viewer): Add UI options to createViewer
    This is redundant if using it along with itkwidgets, since it is also covered there.

--- src/createViewer.js
+++ src/createViewer.js
@@ -198,6 +198,18 @@ const createViewer = async (
       } else {
         options.ui = loadedUIMachineOptions.default
       }
+    } else if (context.uiMachineOptions === 'pydata-sphinx') {
+      options.ui = {
+        href:
+          'https://cdn.jsdelivr.net/npm/itk-viewer-bootstrap-ui@0/dist/itk-viewer-bootstrap-ui.es.js',
+        export: 'default',
+      }
+    } else if (context.uiMachineOptions === 'mui') {
+      options.ui = {
+        href:
+          'https://www.jsdelivr.com/package/npm/itk-viewer-material-ui@0/dist/materialUIMachineOptions.js.es.js',
+        export: 'default',
+      }
     } else {
       options.ui = uiMachineOptions
     }

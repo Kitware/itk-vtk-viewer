628545686e23e761a2af5c45104f8308b5a345bb
Paul Elliott <paul.elliott@kitware.com>
2022-10-14 10:56:24 -0400

    fix(applyColorRange): avoid divide by zero when TF editor point range 0

--- src/Rendering/VTKJS/Images/applyColorRange.js
+++ src/Rendering/VTKJS/Images/applyColorRange.js
@@ -13,7 +13,6 @@ function applyColorRange(context, { data: { name, component, range } }) {
     fullRange = actorContext.colorRangeBounds.get(component)
   }
   const diff = fullRange[1] - fullRange[0]
-
   const colorRangeNormalized = [
     (range[0] - fullRange[0]) / diff,
     (range[1] - fullRange[0]) / diff,
@@ -24,8 +23,12 @@ function applyColorRange(context, { data: { name, component, range } }) {
   const xValues = oldPoints.map(([x]) => x)
   // if 1 point, assume whole range
   const maxOldPoints = xValues.length > 1 ? Math.max(...xValues) : 1
-  const minOldPoints = xValues.length > 1 ? Math.min(...xValues) : 0
-  const rangeOldPoints = maxOldPoints - minOldPoints
+  let minOldPoints = xValues.length > 1 ? Math.min(...xValues) : 0
+  let rangeOldPoints = maxOldPoints - minOldPoints
+  if (rangeOldPoints === 0) {
+    minOldPoints = 0
+    rangeOldPoints = 1
+  }
   const points = oldPoints
     // find normalized position of old points
     .map(([x, y]) => [(x - minOldPoints) / rangeOldPoints, y])

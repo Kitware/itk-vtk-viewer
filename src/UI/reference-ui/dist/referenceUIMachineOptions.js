2f1501b006e8550d64cf4d2c49923c68322c33c7
Paul Elliott <paul.elliott@kitware.com>
2023-12-12 14:40:35 -0500

    fix(extensionToImageIo): remove options that fail roundtrip

--- src/UI/reference-ui/dist/referenceUIMachineOptions.js
+++ src/UI/reference-ui/dist/referenceUIMachineOptions.js
@@ -10738,41 +10738,59 @@ LayerIcon = __decorate([e$7('layer-icon')], LayerIcon)
 
 // from itk-wasm/packages/image-io/typescript/src/extension-to-image-io.ts
 var extensionToImageIo = new Map([
-  ['bmp', 'bmp'],
-  ['dcm', 'gdcm'],
-  ['gipl', 'gipl'],
-  ['gipl.gz', 'gipl'],
+  // ['bmp', 'bmp'],
+
+  // ['dcm', 'gdcm'],
+
+  // ['gipl', 'gipl'],
+  // ['gipl.gz', 'gipl'],
+
   ['hdf5', 'hdf5'],
-  ['jpg', 'jpeg'],
-  ['jpeg', 'jpeg'],
-  ['iwi', 'wasm'],
+  // ['jpg', 'jpeg'],
+  // ['jpeg', 'jpeg'],
+
+  // ['iwi', 'wasm'],
   ['iwi.cbor', 'wasm'],
-  ['iwi.cbor.zst', 'wasmZstd'],
-  ['lsm', 'lsm'],
-  ['mnc', 'mnc'],
-  ['mnc.gz', 'mnc'],
-  ['mnc2', 'mnc'],
-  ['mgh', 'mgh'],
-  ['mgz', 'mgh'],
-  ['mgh.gz', 'mgh'],
+  // ['iwi.cbor.zst', 'wasmZstd'],
+
+  // ['lsm', 'lsm'],
+
+  // ['mnc', 'mnc'],
+  // ['mnc.gz', 'mnc'],
+  // ['mnc2', 'mnc'],
+
+  // ['mgh', 'mgh'],
+  // ['mgz', 'mgh'],
+  // ['mgh.gz', 'mgh'],
+
   ['mha', 'meta'],
-  ['mhd', 'meta'],
-  ['mrc', 'mrc'],
-  ['nia', 'nifti'],
+  // ['mhd', 'meta'],
+
+  // ['mrc', 'mrc'],
+
+  // ['nia', 'nifti'],
   ['nii', 'nifti'],
   ['nii.gz', 'nifti'],
-  ['hdr', 'nifti'],
+  // ['hdr', 'nifti'],
+
   ['nrrd', 'nrrd'],
-  ['nhdr', 'nrrd'],
-  ['png', 'png'],
-  ['pic', 'bioRad'],
+  // ['nhdr', 'nrrd'],
+
+  // ['png', 'png'],
+
+  // ['pic', 'bioRad'],
+
   ['tif', 'tiff'],
-  ['tiff', 'tiff'],
+  // ['tiff', 'tiff'],
+
   ['vtk', 'vtk'],
-  ['isq', 'scanco'],
-  ['aim', 'scanco'],
-  ['fdf', 'fdf'],
+
+  // ['isq', 'scanco'],
+  // ['aim', 'scanco'],
+
+  // ['fdf', 'fdf'],
 ])
+
 var extensions = Array.from(extensionToImageIo.keys())
 
 function createLayerEntry(context, name, layer) {
--- src/UI/reference-ui/src/Layers/extensionToImageIo.js
+++ src/UI/reference-ui/src/Layers/extensionToImageIo.js
@@ -1,57 +1,57 @@
 // from itk-wasm/packages/image-io/typescript/src/extension-to-image-io.ts
 const extensionToImageIo = new Map([
-  ['bmp', 'bmp'],
+  // ['bmp', 'bmp'],
 
-  ['dcm', 'gdcm'],
+  // ['dcm', 'gdcm'],
 
-  ['gipl', 'gipl'],
-  ['gipl.gz', 'gipl'],
+  // ['gipl', 'gipl'],
+  // ['gipl.gz', 'gipl'],
 
   ['hdf5', 'hdf5'],
 
-  ['jpg', 'jpeg'],
-  ['jpeg', 'jpeg'],
+  // ['jpg', 'jpeg'],
+  // ['jpeg', 'jpeg'],
 
-  ['iwi', 'wasm'],
+  // ['iwi', 'wasm'],
   ['iwi.cbor', 'wasm'],
-  ['iwi.cbor.zst', 'wasmZstd'],
+  // ['iwi.cbor.zst', 'wasmZstd'],
 
-  ['lsm', 'lsm'],
+  // ['lsm', 'lsm'],
 
-  ['mnc', 'mnc'],
-  ['mnc.gz', 'mnc'],
-  ['mnc2', 'mnc'],
+  // ['mnc', 'mnc'],
+  // ['mnc.gz', 'mnc'],
+  // ['mnc2', 'mnc'],
 
-  ['mgh', 'mgh'],
-  ['mgz', 'mgh'],
-  ['mgh.gz', 'mgh'],
+  // ['mgh', 'mgh'],
+  // ['mgz', 'mgh'],
+  // ['mgh.gz', 'mgh'],
 
   ['mha', 'meta'],
-  ['mhd', 'meta'],
+  // ['mhd', 'meta'],
 
-  ['mrc', 'mrc'],
+  // ['mrc', 'mrc'],
 
-  ['nia', 'nifti'],
+  // ['nia', 'nifti'],
   ['nii', 'nifti'],
   ['nii.gz', 'nifti'],
-  ['hdr', 'nifti'],
+  // ['hdr', 'nifti'],
 
   ['nrrd', 'nrrd'],
-  ['nhdr', 'nrrd'],
+  // ['nhdr', 'nrrd'],
 
-  ['png', 'png'],
+  // ['png', 'png'],
 
-  ['pic', 'bioRad'],
+  // ['pic', 'bioRad'],
 
   ['tif', 'tiff'],
-  ['tiff', 'tiff'],
+  // ['tiff', 'tiff'],
 
   ['vtk', 'vtk'],
 
-  ['isq', 'scanco'],
-  ['aim', 'scanco'],
+  // ['isq', 'scanco'],
+  // ['aim', 'scanco'],
 
-  ['fdf', 'fdf'],
+  // ['fdf', 'fdf'],
 ])
 
 export const extensions = Array.from(extensionToImageIo.keys())

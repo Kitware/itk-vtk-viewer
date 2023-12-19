cf87e7d8b88e8e33c15e8e4edc23d9d1944eb01b
Paul Elliott <paul.elliott@kitware.com>
2023-12-19 09:56:55 -0500

    fix(mapToColorFunctionRange): only send event if values changed

--- src/Rendering/VTKJS/Images/mapToColorFunctionRange.js
+++ src/Rendering/VTKJS/Images/mapToColorFunctionRange.js
@@ -10,6 +10,15 @@ function mapToColorFunctionRange(
   const rangeDelta = dataRange[1] - dataRange[0]
   const range = points.map(v => v * rangeDelta + dataRange[0])
 
+  // compare with current values to see if updated needed
+  const { colorRanges } = context.images.actorContext.get(name)
+  if (colorRanges.has(component)) {
+    const currentRange = colorRanges.get(component)
+    if (currentRange[0] === range[0] && currentRange[1] === range[1]) {
+      return
+    }
+  }
+
   context.service.send({
     type: 'IMAGE_COLOR_RANGE_CHANGED',
     data: {
